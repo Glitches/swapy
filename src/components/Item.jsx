64028d9e65e111aeacf64bcda1eb759d59e17a47
Andrea Ceccarelli <gltch2003@gmail.com>
2018-01-02 19:00:10 +0100

    Feature: add photo when spec field is empty

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,20 @@
+// Photo is from respoective owner
+// I ABSOLUTELY don't want to break copyright, it's just for my study purpose
+
+
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './Item.css';
-
 import '../assets/akrobat/Akrobat-Regular.otf';
-
-
+import hqdefault from '../assets/hqdefault.jpg'
 // TODO: aggiungere una proprieta nei reducer
 //      dove si lista il singolo veicolo o altro e qui mappa tutte le caratteristiche
 
 const Show = (props) => {
-  let vehic = props.show.show;
+  const vehic = props.show.show;
   console.log(vehic);
-  if (vehic.length === 0) return (<p>ciao</p>);
+  if (vehic.length === 0) return (<img src={hqdefault} alt={"hqdefualt"} />);
   return (
     // console.log(props.show.vehicles[vehic])
     Object.keys(props.show.vehicles[vehic]).map((item, i) => (
@@ -18,5 +22,10 @@ const Show = (props) => {
     ))
   );
 };
+
+Show.propTypes = {
+  show: PropTypes.object,
+};
+
 export default Show;
 
